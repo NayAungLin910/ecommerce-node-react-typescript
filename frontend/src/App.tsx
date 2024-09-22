68ab4e27078ca917a0d2e9653dad67c22084c620
Nay Aung Lin <nayaung910@gmail.com>
2024-09-22 12:07:52 +0630

    Add ReduxStore, ReduxPersist

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 
 function App() {
+  const user = useSelector((state: RootState) => state.user.currentUser);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/products/:category" Component={ProductList} />
+        <Route path="/product/:id" Component={Product} />
+        <Route path="/cart" Component={Cart} />
+        <Route path="/success" Component={Success} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,39 @@
+import { combineReducers, configureStore, Reducer } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import userReducer from "./userRedux";
+import {
+  FLUSH,
+  PAUSE,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedRootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PURGE, REGISTER],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+
+export let persistor = persistStore(store);
