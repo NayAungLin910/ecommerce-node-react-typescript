ede9dfacb7147a991d3b44c1bdfa554d892455d1
Nay Aung Lin <nayaung910@gmail.com>
2024-09-24 23:28:02 +0630

    Add Navbar

--- frontend/src/components/css-helper/css-helper.ts
+++ frontend/src/components/css-helper/css-helper.ts
@@ -1,1 +1,9 @@
-import { css } from "styled-components"
+import { css, CSSObject } from "styled-components";
+
+export const mobile = (props: CSSObject) => {
+  return css`
+    @media only screen and (max-width: 380px) {
+      ${props}
+    }
+  `;
+};
