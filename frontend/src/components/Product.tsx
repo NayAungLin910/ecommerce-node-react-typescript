d4457ee040518cfde4769372636ee62063828279
Nay Aung Lin <nayaung910@gmail.com>
2024-09-28 11:09:26 +0630

    Add Product Page

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,13 +1,98 @@
 import { FC } from "react";
+import styled from "styled-components";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { MdOutlineFavorite } from "react-icons/md";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
 
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
 
-const Product: FC<{}> = ({}) => {
-    return (
-        <>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste soluta incidunt voluptatem, architecto nulla consequatur. Deleniti nihil enim itaque commodi dolorem nulla impedit possimus odio rem, totam, nesciunt consequuntur sint.
-        </>
-    )
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
+
+export interface ProductPropsInterface {
+  item: {
+    _id: string;
+    img: string;
+  };
 }
 
-export default Product
+const Product: FC<ProductPropsInterface> = ({ item }) => {
+  return (
+    <Container>
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon>
+          <FaShoppingCart />
+        </Icon>
+        <Icon>
+          <Link to={`/product/${item._id}`}>
+            <IoSearchOutline />
+          </Link>
+        </Icon>
+        <Icon>
+          <MdOutlineFavorite />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
+
+export default Product;
